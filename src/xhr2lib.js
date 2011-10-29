fbb01ed71f839b844e40b49f7e3b8af56bfcbb6d
Phil Parsons <phil@profilepicture.co.uk>
2011-10-29 14:05:01 +0100

    some progress and clean up

--- src/xhr2lib.js
+++ src/xhr2lib.js
@@ -1,224 +1,392 @@
-/**
-	@author <a href="http://www.profilepicture.co.uk">Phil Parsons</a>
-	@namespace xhr2-lib
-	@class
-*/
-var xhr2 = (function () {
+(function (w) {
+  
+  "use strict";
+  
+  /**
+    @author <a href="http://www.profilepicture.co.uk">Phil Parsons</a>
+    @namespace xhr2
+    @class
+  */
+  w.$xhr = (function () {
+  
+    var exp = {}; // module api
+        
+    
+    // :ajax api
+  
+    /**
+      Convenience function for simple HTML get
+      requests
+  
+      @public
+      @param {String} url destination URL
+      @param {Object} [data] request params
+      @param {Function} [cb] success callback function 
+    */
+    exp.get = function () {
+      
+      var opts = _createOptions(arguments);
+      opts.url += exp.serializeQS(opts.url, opts.data);
+      
+      exp.ajax(opts);
+      
+    };
+    
+  
+    /**
+      Convenience function for simple JSON get
+      requests
+  
+      @public
+      @param {String} url destination URL
+      @param {Object} [data] request params
+      @param {Function} [cb] success callback function 
+    */
+    exp.getJSON = function () {
+      
+      var opts = _mix(_createOptions(arguments), {dataType: "json"});
+      opts.url += exp.serializeQS(opts.url, opts.data);
+      
+      exp.ajax(opts);
+      
+    };
+    
+  
+    /**
+      Convenience function for simple XML get
+      requests
+  
+      @public
+      @param {String} url destination URL
+      @param {Object} [data] request params
+      @param {Function} [cb] success callback function 
+    */
+    exp.getXML = function () {
+      
+      var opts = _mix(_createOptions(arguments), {dataType: "xml"});
+      opts.url += exp.serializeQS(opts.url, opts.data);
+      
+      exp.ajax(opts);
+      
+    };
+       
+  
+    exp.post = function () {
+      
+      exp.ajax(
+        _mix(createOptions(arguments), {type: "post"})
+      );
+      
+    };
+    
+  
+    /**
+     * Creates formdata object and sends to the 
+     * request method as data
+     */
+     exp.sendForm = function () {
+       
+      var fd
+        , options
+        , f = arguments[0]
+        , args = Array.prototype.slice.call(arguments, 1);
+       
+      if (f && f.nodeName === "FORM") {
+          
+        fd = new FormData(f);
+        options = _mix(
+            _createOptions(args)
+          , { url: f.action || w.location.href, type: "post", data: fd }
+        );
+        
+        exp.ajax(options);
+        
+      } 
+      
+      else throw new Error("Expected form as first argument");
+      
+    };
+    
+    /**
+       
+     
+    */
+    exp.sendFile = function (url, f, success) {
+    	
+    	exp.ajax({
+    	   url: url + "?isFile"
+    	 , data: f
+    	 , success: success
+    	 , type: "post"
+    	 , headers: {
+      	   "X-File-Name": f.name
+      	 }
+    	});
+    	
+    };
+    
+    
+    /**
+      main ajax method
+  
+      @private
+      @param {Object} opts The ajax HTTP request options
+    */
+    exp.ajax = function (opts) {
+      
+      var client = new XMLHttpRequest()
+        , upload
+        , defaults = { // default request parameters
+              progress: false
+            , type: "get"
+            , dataType: "html"
+          }
+        , opts = _mix(defaults, opts);
+        
+      // progress event listener
+      if (opts.progress && typeof opts.progress === "function") {
+        
+        upload = client.upload;
+        
+        if (upload) {
+          upload.callback = opts.progress;
+          upload.addEventListener("progress", _progress, false);
+        }
+        
+      }
+      
+      if (opts.timeout && typeof opts.timeout === "number") {
+        client.timeout = opts.timeout;
+      }
+      
+      // create connection
+      client.open(opts.type, opts.url);
+      _headers(client, opts.dataType, opts.headers);
+      
+      // cache options and callback function
+      client.xhr2data = opts;
+      client.callback = opts.success;
+      
+      // client event handlers
+      client.onreadystatechange = _stateChange;
+      client.onerror = _requestError;
+      
+      //send request
+      client.send(opts.data);
+      
+    };
+    
+    
+    // :Utils api
+    
+    /**
+      Serialises a data object into a query string
+  
+      @public
+      @param {String} url the destination url
+      @param {Object} data the request data
+  
+      @returns {String} data object serialised
+    */
+    exp.serializeQS = function (url, data) {
+      
+      var qs = "";
+      
+      if (typeof data === "object") {
+        
+        if (!~url.search(/\?/)) {
+          qs = "?";
+        } 
+        else {
+          qs = "&";
+        }
+        
+        for (var key in data) {
+          if (data.hasOwnProperty(key)) {
+            qs += key + "=" + data[key] + "&";
+          }
+        }
+        
+        qs = qs.substr(0, qs.length - 1);
+        
+      }
+      
+      return qs;
+      
+    };
+    
+    
+    // :private    
+    
+    var _createOptions = function (args) {
+      
+      var i = 0
+        , opts = { url: args[0] }
+        , params = Array.prototype.slice.call(args, 1);
+        
+      for (; i < params.length; ++i) {
+        
+        switch (typeof params[i]) {
+          
+          case "string":
+            opts.dataType = params[i];
+            break;
+            
+          case "function": // success callback function
+            opts.success = params[i];
+            break;
+            
+          case "object": // data object
+            opts.data = params[i];
+          
+        }
+        
+      }
+      
+      return opts;
+      
+    };
+    
+  
+    /**
+      Mixes passed in options with the defaults
+  
+      @private
+    */
+    var _mix = function () {
+      
+      var ret = {}
+        , i = 0
+        , key;
+      
+      // have options to mix into defaults
+      if (arguments.length && typeof arguments[0] !== "undefined") {
+        
+        for (; i < arguments.length; ++i) { // each option set
+          
+          for (key in arguments[i]) {
+            
+            if (arguments[i].hasOwnProperty(key)) {
+              ret[key] = arguments[i][key];
+            }
+            
+          }
+          
+        }
+        
+      }
+      
+      return ret;
+      
+    };
+    
+  
+    /**
+      Event handler for readystatechange events
+      Fires callback, if one was defined, with
+      formatted data as required
+  
+      @private
+    */
+    var  _stateChange = function (ev) {
+      
+      var resBody
+        , xhr = ev.target
+        , clientData = xhr ? xhr.xhr2data : undefined;
+        
+      if ((xhr && xhr.callback) && clientData) {
+        
+        if (xhr.readyState === 4) {
+          
+          if (clientData.dataType === "json") {
+            try {
+              resBody = JSON.parse(xhr.responseText);
+            } catch (ex) {
+              // if invalid json data - don't error, just pass response body
+              resBody = xhr.responseText;
+            }
+          }
+          
+          else if (clientData.dataType === "xml") {
+            resBody = xhr.responseXML;
+          }
+          
+          else {
+            resBody = xhr.responseText;
+          }
+          
+          xhr.callback.call(xhr, resBody);
+          
+        }
+        
+      }
+      
+    };
+  
+    /*
+      Fires upload progress event handler with percentage
+      of upload complete and total
+  
+      @private
+    */
+    var  _progress = function (ev) {
+      
+      if (ev && ev.lengthComputable) {
+        // call progress handler with pct loaded
+      }
+      
+    };
+  
+    /**
+      Add request headers including accept mime
+  
+      @private
+    */
+    var  _headers = function (client, resType, headers) {
+      
+      var mime = ""
+        , resType = resType.toLowerCase()
+        , header;
+      
+      if (resType === "json") {
+        mime += "application/json, ";
+      } 
+      else if (resType === "xml") {
+        mime += "text/xml, ";
+      }
+      mime += " text/html, */*";
+      
+      client.setRequestHeader("Accept", mime);
+      client.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+      client.setRequestHeader("Cache-Control", "No-Cache");
+      
+      if (typeof headers === "object") { // user defined headers
+        
+        for (header in headers) {
+          if (headers.hasOwnProperty(header)) {
+            client.setRequestHeader(header, headers[header]);
+          }
+        }
+        
+      }
+      
+    };
+  
+    /**
+      Default error handler for requests
+      
+      @private
+      
+      @param {XMLHttpRequest} xhr The failed request object
+     */
+    var  _requestError = function (xhr) {
+      
+      console.log("error: ", xhr);
+      
+    };
+  
+    return exp; // export api
+  
+  })();
 
-	var exp = {};
-
-	var defaults = {
-			progress: false
-		,	type: "GET"
-		,	dType: "html"
-	};
-
-	/**
-		Convenience function for simple HTML get
-		requests
-
-		@public
-		@param {String} url destination URL
-		@param {Object} [data] request params
-		@param {Function} [cb] success callback function 
-	*/
-	exp.get = function (url, data, cb) {
-		var qs = exp.serializeQS(url, data);
-		_getRequest(url + qs, defaults, data, cb);
-	}
-
-	/**
-		Convenience function for simple JSON get
-		requests
-
-		@public
-		@param {String} url destination URL
-		@param {Object} [data] request params
-		@param {Function} [cb] success callback function 
-	*/
-	exp.getJSON = function (url, data, cb) {
-		var qs = exp.serializeQS(url, data)
-			,	opts = _mix({dType: "json"});
-		_getRequest(url + qs, opts, data, cb);
-	}
-
-	/**
-		Convenience function for simple XML get
-		requests
-
-		@public
-		@param {String} url destination URL
-		@param {Object} [data] request params
-		@param {Function} [cb] success callback function 
-	*/
-	exp.getXML = function (url, data, cb) {
-		var qs = exp.serializeQS(url, data)
-			,	opts = _mix({dType: "xml"});
-		_getRequest(url + qs, opts, data, cb);
-	}
-
-	/**
-		Serialises a data object into a query string
-
-		@public
-		@param {String} url the destination url
-		@param {Object} data the request data
-	*/
-	exp.serializeQS = function (url, data) {
-		var qs = "";
-		if (typeof data === "object") {
-			if (!~url.search(/\?/)) {
-				qs = "?";
-			} else {
-				qs = "&";
-			}
-			for (var key in data) {
-				qs += key + "=" + data[key] + "&";
-			}
-			qs = qs.substr(0, qs.length - 1);
-		}
-		return qs;
-	}
-
-	exp.post = function (url, opts, data, cb) {
-		_request(url, opts, (data || {}), cb);
-	}
-	
-	var _getRequest = function () {
-		// No data just callback
-		if (typeof arguments[2] === "function") {
-			_request(arguments[0], arguments[1], undefined, arguments[2]);
-		// data and callback / data and no callback / neither
-		} else {
-			_request(arguments[0], arguments[1], arguments[2], arguments[3]);
-		}
-	}
-
-	/**
-		Creates the request and attaches all event handlers
-
-		@private
-		@param {String} url destination url
-		@param {Object} opts request options
-		@param {FormData|File|Blob} [data] request data
-		@param {function} [cb] callback function on success
-	*/
-	var	_request = function (url, opts, data, cb) {
-		var client = new XMLHttpRequest()
-			,	params = _mix(opts)
-			,	upload;
-		if (params.progress && typeof params.progress === "function") {
-			upload = client.upload;
-			if (upload) {
-				upload.callback = params.progress;
-				upload.addEventListener("progress", _progress, false);
-			}
-		}
-		client.open(params.type, url);
-		_addHeaders(client, params.dType);
-		client.xhr2data = params;
-		client.callback = cb;
-		client.addEventListener(
-				"readystatechange"
-			,	_stateChange
-			,	false
-		);
-		client.addEventListener(
-				"error"
-			,	_handleRequestError
-			,	false
-		);
-		client.send(data);
-	}
-
-	/**
-		Mixes passed in options with the defaults
-
-		@private
-	*/
-	var _mix = function () {
-		var ret = {}
-			,	i = 0
-			,	key;
-		for (key in defaults) {
-			ret[key] = defaults[key];
-		}
-		if (arguments.length && typeof arguments[0] !== "undefined") {
-			for (; i < arguments.length; ++i) {
-				for (key in arguments[i]) {
-					ret[key] = arguments[i][key];
-				}
-			}
-		}
-		return ret; 
-	}
-
-	/**
-		Event handler for readystatechange events
-		Fires callback, if one was defined, with
-		formatted data as required
-
-		@private
-	*/
-	var	_stateChange = function (ev) {
-		var xhr = ev.target
-			,	clientData = xhr ? xhr.xhr2data : undefined;
-		if (clientData) {
-			if (xhr.readyState === 4) {
-				if (clientData.dType === "json") {
-					try {
-						resBody = JSON.parse(xhr.responseText);
-					} catch (ex) {
-						// invalid json data, just pass response body
-						resBody = xhr.responseText;
-					}
-				} else if (clientData.dType === "xml") {
-					resBody = xhr.responseXML;
-				} else {
-					resBody = xhr.responseText;
-				}
-				if (xhr.callback) {
-					xhr.callback.call(undefined, resBody);
-				}
-			}
-		}
-	}
-
-	/*
-		Fires upload progress event handler with percentage
-		of upload complete and total
-
-		@private
-	*/
-	var	_progress = function (ev) {
-		console.log(ev);
-		if (ev && ev.lengthComputable) {
-			// call progress handler with pct loaded
-		}
-	}
-
-	/**
-		Add request headers including accept mime
-
-		@private
-	*/
-	var	_addHeaders = function (client, resType) {
-		var mime = "*/*, text/html"
-			,	resType = resType.toLowerCase();
-		client.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-		client.setRequestHeader("Cache-Control", "No-Cache");
-		if (resType === "json") {
-			mime += ", application/json";
-		} else if (resType === "xml") {
-			mime += ", text/xml";
-		}
-		client.setRequestHeader("Accept", mime);
-	}
-
-	var	_handleRequestError = function (xhr) {
-		console.log(xhr);
-	}
-
-	return exp;
-
-})();
+ })(window);
--- test/test.js
+++ test/test.js
@@ -1,68 +1,140 @@
+module("$xhr");
+
 test("serializeQS()", function () {
-	var url1 = "http://www.example.com";
-	var url2 = "http://www.example.com?withqs=true";
-	equal(
-			xhr2.serializeQS(url1, {paramOne: "test",	paramTwo: "test"})
-		,	"?paramOne=test&paramTwo=test"
-		,	"Url without query string"
-	);
-	equal(
-			xhr2.serializeQS(url2, {paramOne: "test",	paramTwo: "test"})
-		,	"&paramOne=test&paramTwo=test"
-		,	"Url with query string"
-	);
-	equal(
-			xhr2.serializeQS(url1, {})
-		,	""
-		,	"Empty data object"
-	);
+
+  var url1 = "http://www.example.com"
+    , url2 = "http://www.example.com?withqs=true";
+
+  equal(
+      $xhr.serializeQS(url1, {paramOne: "test",  paramTwo: "test"})
+    , "?paramOne=test&paramTwo=test"
+    , "Url without query string"
+  );
+
+  equal(
+      $xhr.serializeQS(url2, {paramOne: "test",  paramTwo: "test"})
+    , "&paramOne=test&paramTwo=test"
+    , "Url with query string"
+  );
+
+  equal(
+      $xhr.serializeQS(url1, {})
+    , ""
+    , "Empty data object"
+  );
+
 });
 
-asyncTest("get()", function () {
-	xhr2.get(
-			"server.php?testget"
-		,	{
-					title: "Testing get()"
-				,	msg: "The test has passed"
-			}
-		,	function (html) {
-				equal(
-						html.replace(/\s/g, "")
-					,	"<header><h1>Testingget()</h1></header>" + 
-						"<section><p>Thetesthaspassed</p></section>" + 
-						"<footer><p>&copy;2011</p></footer>"	
-					,	"needs a compare test here"
-				);
-				start();
-			}
-	);
+asyncTest("get", function () {
+
+  $xhr.get(
+      "server.php?testget"
+    , {
+          title: "Testing get()"
+        , msg: "The test has passed"
+      }
+    , function (html) {
+
+        equal(
+            html.replace(/\s/g, "")
+          , "<header><h1>Testingget()</h1></header>" + 
+            "<section><p>Thetesthaspassed</p></section>" + 
+            "<footer><p>&copy;2011</p></footer>"  
+          , "needs a compare test here"
+        );
+
+        start();
+
+      }
+  );
+  
 });
 
 asyncTest("getJSON", function () {
-	xhr2.getJSON(
-			"server.php?testgetjson"
-		,	function (data) {
-				deepEqual(
-						data
-					,	[
-								{
-										firstname: "John"
-									,	lastname: "Smith"
-									,	occupation: "Plumber"
-								}
-							,	{
-										firstname: "Jane"
-									,	lastname: "Smith"
-									,	occupation: "Accountant"
-								}
-							,	{
-										firstname: "David"
-									,	lastname: "Bowe"
-									,	occupation: "Singer"
-								}
-						]
-					,	"No data object passed just callback function"
-				);
-				start();
-		});
+
+  $xhr.getJSON(
+      "server.php?testgetjson"
+    , function (data) {
+
+        deepEqual(
+            data
+          ,  [
+                {
+                    firstname: "John"
+                  , lastname: "Smith"
+                  , occupation: "Plumber"
+                }
+              , {
+                    firstname: "Jane"
+                  , lastname: "Smith"
+                  , occupation: "Accountant"
+                }
+              , {
+                    firstname: "David"
+                  , lastname: "Bowe"
+                  , occupation: "Singer"
+                }
+            ]
+          , "No data object passed just callback function"
+        );
+
+        start();
+
+    });
+
+});
+
+asyncTest("getXML", function () {
+  
+  $xhr.getXML("tes.xml", function (xml) {
+    
+    equal(1, 0);
+    start();
+    
+  });
+  
+});
+
+asyncTest("ajax", function () {
+  
+   $xhr.ajax({
+      url: "server.php?testgetjson"
+    , timeout: 1
+    , success: function (res) {
+        console.log(this);
+        start();
+      }
+    , dataType: "json"
+   });
+  
+})
+
+var tf = document.getElementById("testForm");
+
+tf.addEventListener("submit", function (ev) {
+
+  ev.preventDefault();
+
+  $xhr.postForm(tf, {}, function (res) {
+    var elem = document.createElement('p');
+    elem.innerHTML = res;
+    tf.appendChild(elem);
+  });
+
+}, false);
+
+var sf = document.getElementById("send-file");
+
+sf.addEventListener("click", function (ev) {
+  
+  ev.preventDefault();
+  
+  var f = document.getElementById("file-field").files[0];
+  
+  $xhr.sendFile("server.php", f, function (res) {
+    
+    console.log(res);
+    
+  });
+  
 });
